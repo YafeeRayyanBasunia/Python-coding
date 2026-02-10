91b682941bb4086bb4412ee7133201bba5da7d24
YafeeRayyanBasunia <noushin9999@gmail.com>
2026-02-10 18:38:30 +0600

    Add files via upload

---
+++ alphabet or digit.py
@@ -0,0 +1,5 @@
+ld = int(input("type in here a leter or a digit: "))
+if ld in  "ABCDEFGHIJKLMNOPQSTUVWXYZabcdefghijklmnopqrstuvwxyz":
+    print("It is an alphabet")
+else:
+    print("It is not an alphabet: ")    
